54e128a4e5582fb8c1be9c8b7b69cec7499683f4
banhbao.beongay132@gmail.com <Trinhvulam1!@#>
2022-11-26 15:07:48 +0700

    shibe code

--- thuc hanh 8/src/bai1/hammain.java
+++ thuc hanh 8/src/bai1/hammain.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class hammain {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-//        System.out.print("Nhap vao ki tu : ");
-//        String str=sc.nextLine();
-        VanBan st = new VanBan("          cai   dit con    me   ");
+        System.out.print("Nhap vao ki tu : ");
+        String str=sc.nextLine();
+        VanBan st = new VanBan(str);
         System.out.println("So tu trong chuoi la : "+st.DemSoTu());
-        System.out.println("So tu ket thuc bang G: "+st.DemSoTu_ket_thuc_boi_G());
+        System.out.println("So tu ket thuc bang G : "+st.DemSoTu_ket_thuc_boi_G());
         System.out.println("Truoc khi chuan hoa : "+st.prinnnt());
         System.out.print("Sau khi chuan hoa : ");st.ChuanHoa();
 
